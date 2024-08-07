aaaa9c1eb97cccfa66f60a4e5a78a367152cfffd
John Wendeborn <jwendeborn@gmail.com>
2024-08-07 15:35:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    install_reqs = f.read().splitlines()
+
 setup(
       name = 'Lightcurvy',
       version = '0.2',
       description = 'Scripts to fetch astronomical light curves from a variety of sources.',
       author = 'John Carlos Wendeborn',
       author_email = 'jwendeborn@gmail.com',
-      install_requires = ['numpy', 'pandas', 'matplotlib', 'astroquery', 'astropy', 'tglc', 'eleanor', 'lightkurve', 'tess-point', 'scikit-learn', 'requests'],
+      install_requires = install_reqs,
       license = 'MIT',
       url = 'https://github.com/wendeborn8/lightcurvy',
-      
       )
